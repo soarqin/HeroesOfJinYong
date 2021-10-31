080c3a283b9efc6995dcbca3fb20c43660e5ba81
boluny <yuanbl0605@163.com>
2021-11-01 00:32:57 +0800

    Fix main menu: * positions error for volume setting; * default option when exit game;

--- src/scene/window.cc
+++ src/scene/window.cc
@@ -809,7 +809,7 @@ void optionMenu(Node *mainMenu, int x, int y) {
                 }
                 core::config.setMusicVolume(val);
                 audio::gMixer.setVolume(0, 16 * val);
-                subMenu->setValue(1, fmt::format(L" {:>2}", val));
+                subMenu->setValue(2, fmt::format(L" {:>2}", val));
                 break;
             }
             case 3: {
@@ -822,7 +822,7 @@ void optionMenu(Node *mainMenu, int x, int y) {
                     ++val;
                 }
                 core::config.setSoundVolume(val);
-                subMenu->setValue(2, fmt::format(L" {:>2}", val));
+                subMenu->setValue(3, fmt::format(L" {:>2}", val));
                 break;
             }
             default:
